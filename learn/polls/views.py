f8bc692d535fe224615c1757de1747fa6ae2ceb5
shellshock1953 <shellshock1953@gmail.com>
2016-10-10 00:12:24 +0300

    djano way

--- learn/polls/views.py
+++ learn/polls/views.py
@@ -1,24 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choise
 
-from django.template import RequestContext, loader
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
-        context = {'latest_question_list': latest_question_list}
-        return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request, "polls/detail.html", {"question":question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def result(request, question_id):
-    response = "You`re looking at question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
-    return HttpResponse("You`re voting at question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choise = question.choise_set.get(pk=request.POST['choise'])
+    except (KeyError, Choise.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message':"You didnt select a choise",
+        })
+    else:
+        selected_choise.votes += 1
+        selected_choise.save()
+        return HttpResponse()
 
